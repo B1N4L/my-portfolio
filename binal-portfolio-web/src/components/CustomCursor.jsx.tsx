38a49ab9a551fc0f0e9acc634375d01e1e250e74
B1N4L <163591915+B1N4L@users.noreply.github.com>
2026-05-09 09:09:35 +0530

    fix(cursor): mouse cursor event when hover input elements and buttons

--- binal-portfolio-web/src/components/CustomCursor.jsx.tsx
+++ binal-portfolio-web/src/components/CustomCursor.jsx.tsx
@@ -42,20 +42,35 @@ export default function CustomCursor({ label = "You" }: CustomCursorProps) {
         >
             <div className="relative">
                 {/* Pointer icon */}
+                {/*<svg*/}
+                {/*    width="18"*/}
+                {/*    height="24"*/}
+                {/*    viewBox="0 0 18 24"*/}
+                {/*    className="block drop-shadow-sm"*/}
+                {/*>*/}
+                {/*    <path d="M2 1 L2 18 L13 11 Z" fill="white" />*/}
+                {/*</svg>*/}
                 <svg
-                    width="18"
-                    height="24"
-                    viewBox="0 0 18 24"
+                    width="16"
+                    height="20"
+                    viewBox="0 0 331 434"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
                     className="block drop-shadow-sm"
                 >
                     <path
-                        d="M2 1 L2 18 L6.5 14.5 L9.5 22 L12 21 L9 13.5 L16 13 Z"
-                        fill="white"
+                        d="M329.468 243.763L118.477 254.976L118.158 254.993L118.039 255.289L47.476 431.949L0.620072 1.07913L329.468 243.763Z"
+                        fill="#ffffff"
+                    />
+                    <path
+                        d="M329.468 243.763L118.477 254.976L118.158 254.993L118.039 255.289L47.476 431.949L0.620072 1.07913L329.468 243.763Z"
+                        // stroke="#000000"
+                        // strokeWidth="18"
                     />
                 </svg>
 
                 {/* Label bubble */}
-                <div className="absolute left-4 top-3 rounded-md bg-white px-2 py-1 text-xs font-medium text-black shadow-sm">
+                <div className="absolute left-6 top-3 rounded-md bg-white px-2 py-1 text-xs font-medium text-black shadow-sm">
                     {label}
                 </div>
             </div>
